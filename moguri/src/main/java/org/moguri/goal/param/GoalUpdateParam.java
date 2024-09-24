fdf997263bd6250ffcbd893c63bfb66a0b771714
Eunji Lee <165126597+runeunrun@users.noreply.github.com>
2024-09-24 11:47:47 +0900

    [MOGURI-6] fix : resolve merge problem

---
+++ moguri/src/main/java/org/moguri/goal/param/GoalUpdateParam.java
@@ -0,0 +1,23 @@
+package org.moguri.goal.param;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class GoalUpdateParam {
+
+    private String goalName;
+    private BigDecimal goalAmount;
+    private BigDecimal currentAmount;
+    private Date startDate;
+    private Date endDate;
+
+
+}
